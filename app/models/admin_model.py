5ed76eb42ddc61ffbc3d98e85677f6801553e0fb
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-05-18 00:33:43 +0100

    feat: add admin model

---
+++ app/models/admin_model.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+
+class Admin(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+        self.email = user_data['email']
+        self.password = user_data['password']
