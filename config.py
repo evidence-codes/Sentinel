1fe06263a6e2affd34110aa2a34d73162d90a59e
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-05-18 00:30:48 +0100

    feat: add app secret

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 
 class Config:
-    SECRET_KEY = ""
+    SECRET_KEY = "bd0467ad425dd8508a78619a393502584d9aa6b2"
     MONGO_URI = "mongodb+srv://Evidence:DsCw2hUue1NkvtHJ@sel-predict.w2jdrd4.mongodb.net/test?retryWrites=true&w=majority&appName=sel-predict"
