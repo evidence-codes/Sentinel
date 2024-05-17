4bc216519c8d3c7af41009df183aca1f304b63c9
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-05-18 00:33:29 +0100

    feat: register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from flask_pymongo import PyMongo
 from config import Config
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 mongo = PyMongo()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
+    
     mongo.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message_category = 'info'
+    
     
     # Log connection status
     with app.app_context():
@@ -16,9 +27,18 @@ def create_app():
             mongo.db.command('ping')
             app.logger.info("Successfully connected to MongoDB")
         except Exception as e:
-            app.logger.error(f"Error connecting to MongoDB: {e}")
+            app.logger.error(f"Error connecting to MongoDB: {e}")     
+    
 
     from .routes import main
     app.register_blueprint(main)
+    
+    # print("Registering auth blueprint...")
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    # print("Auth blueprint registered.")
+    
+    # for rule in app.url_map.iter_rules():
+    #     print(rule)
 
     return app
