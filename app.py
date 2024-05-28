87b79c99d22113c430ca7a1d00def34968c5747b
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-05-28 10:39:23 +0100

    feat: change deployment to dev

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from waitress import serve
+# from waitress import serve
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=8000)
+    # serve(app, host="0.0.0.0", port=8000)
+    app.run(debug=True)
