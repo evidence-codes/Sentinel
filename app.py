454ea82dc4c92983bed3ffabf0f7f8d058e1f299
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-05-30 18:34:41 +0100

    feat: add CORS to accept all origins

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # from waitress import serve
 from app import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 if __name__ == '__main__':
     # serve(app, host="0.0.0.0", port=8000)
