1baf94f2eadd17e7a37eb3a597c5678cfdc0baaa
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-06-08 22:22:40 +0100

    feat: serve react build on server

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,15 @@ from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from .routes import main as main_blueprint
+from bson import ObjectId
+from .models.user_model import User
 
 mongo = PyMongo()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
 def create_app():
-    app = Flask(__name__, static_folder='static')
+    app = Flask(__name__, static_folder='static/assets', template_folder='static')
     CORS(app)
     app.config.from_object(Config)
     
@@ -30,29 +32,37 @@ def create_app():
             mongo.db.command('ping')
             app.logger.info("Successfully connected to MongoDB")
         except Exception as e:
-            app.logger.error(f"Error connecting to MongoDB: {e}")     
+            app.logger.error(f"Error connecting to MongoDB: {e}")  
+            
+     
+    @login_manager.user_loader
+    def load_user(user_id):
+        user_data = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+        if not user_data:
+            return None
+        return User(user_data)           
     
 
     app.register_blueprint(main_blueprint)
     
     # print("Registering auth blueprint...")
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/api/auth')
     
     from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    app.register_blueprint(admin_blueprint, url_prefix='/api/admin')
     
     from .predictions import predict as predict_blueprint
-    app.register_blueprint(predict_blueprint, url_prefix='/predict')
+    app.register_blueprint(predict_blueprint, url_prefix='/api/predict')
     
     from .device import device as device_blueprint
-    app.register_blueprint(device_blueprint, url_prefix='/device')
+    app.register_blueprint(device_blueprint, url_prefix='/api/device')
     
     from .data import train as train_blueprint
-    app.register_blueprint(train_blueprint, url_prefix='/data')
+    app.register_blueprint(train_blueprint, url_prefix='/api/data')
     
     from .user import user as user_blueprint
-    app.register_blueprint(user_blueprint,url_prefix='/user')
+    app.register_blueprint(user_blueprint,url_prefix='/api/user')
     # print("Auth blueprint registered.")
     
     # for rule in app.url_map.iter_rules():
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, send_from_directory, render_template
 import os
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-def home():
-    # return send_from_directory(os.path.join(main.root_path, 'static'), 'index.html')
-    return "Welcome to the Flask app!"
+# @main.route('/')
+# def home():
+#     # return send_from_directory(os.path.join(main.root_path, 'static'), 'index.html')
+#     return "Welcome to the Flask app!"
 
-@main.route('/about')
-def about():
-    return "This is the about page."
+# @main.route('/about')
+# def about():
+#     return "This is the about page."
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/<path:path>')
+def static_proxy(path):
+    file_name = path.split('/')[-1]
+    dir_name = os.path.join(main.static_folder, '/'.join(path.split('/')[:-1]))
+    return send_from_directory(dir_name, file_name)
